7b10b1533a969e45687a3bfc8694e4ad4fba7fdf
guikrusemark <guilhermekrusemark@gmail.com>
2023-12-29 10:15:41 -0300

    document-handling

--- sandbox/web-scraping/selenium/automation.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver import Chrome
-
-url = 'https://curso-python-selenium.netlify.app/exercicio_01.html'
-
-browser = Chrome()
-browser.get(url)
-
-browser.find_element_by_tag_name('a').click()
