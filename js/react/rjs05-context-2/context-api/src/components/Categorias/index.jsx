3183651ac07bcc72d9a3ec7774ac3db0e164cace
guikrusemark <guilhermekrusemark@gmail.com>
2024-04-28 21:37:32 -0300

    Start lecture

---
+++ js/react/rjs05-context-2/context-api/src/components/Categorias/index.jsx
@@ -0,0 +1,25 @@
+import Titulo from "@/components/Titulo";
+import categorias from "@/mocks/categorias.json";
+import React from "react";
+import Categoria from "./Categoria";
+
+const Categorias = () => {
+	return (
+		<section role="categorias" aria-label="Busque por categoria:">
+			<Titulo>Busque por categoria:</Titulo>
+
+			<div className="container row mx-auto mb-5 g-4">
+				{categorias.map((categoria) => (
+					<Categoria
+						key={categoria.id}
+						alt={categoria.alt}
+						src={categoria.src}
+						descricao={categoria.descricao}
+					/>
+				))}
+			</div>
+		</section>
+	);
+};
+
+export default Categorias;
